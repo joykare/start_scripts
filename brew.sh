99fa3bf346e552f93b1278738167c3fcd4bdba9b
joykare <warugujoy@gmail.com>
2017-06-22 15:32:26 +0300

    Add start scripts initial commit

---
+++ brew.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+brew install git
+
+brew cask install google-chrome
+brew cask install docker
+brew cask install slack
+brew cask install spotify
+brew cask install tunnelbear
+brew cask install vlc
+brew cask install visual-studio-code
+brew cask install iterm2
